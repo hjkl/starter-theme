2ba37e7187aa5fe022076d794b8d321d8ef7e818
Mark <mark@rdit.co.uk>
2018-12-03 10:19:54 +0000

    Add use statement for Timber Menu

--- functions.php
+++ functions.php
@@ -11,6 +11,7 @@
 use Timber\Post;
 use Timber\Site;
 use Timber\Timber;
+use Timber\Menu;
 
 
 if ( ! class_exists( 'Timber\Timber' ) ) {
@@ -68,7 +69,7 @@ class StarterSite extends Site {
 		$context['foo'] = 'bar';
 		$context['stuff'] = 'I am a value set in your functions.php file';
 		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
-		$context['menu'] = new Timber\Menu();
+		$context['menu'] = new Menu();
 		$context['site'] = $this;
 		return $context;
 	}
